168c73cdad8971640c1983f3660c75c6d17e83fe
Marcos Andrade <marcosjuremao@hotmail.com>
2022-07-02 23:32:01 -0300

    primeiro commit

---
+++ SalesWebMvc/SalesWebMvc/Controllers/DepartamentosController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SalesWebMvc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMvc.Controllers
+{
+    public class DepartamentosController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Departamento> lista = new List<Departamento>();
+            lista.Add(new Departamento { Id = 1, Name = "Eletronico" });
+            lista.Add(new Departamento { Id = 2, Name = "cozinha" });
+            return View(lista);
+        }
+    }
+}
